f93cc91cb436861d2099888ad23011cb4d5d610d
Muhammad Umer Riaz <exclusiveumer@gmail.com>
2023-11-07 19:03:51 +0500

    Navbar & Main-Content done

---
+++ my-app/src/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <div>
+            <img src="/AirBnblogo.jpg" alt="bnbLogoError" className="bnbLogo" />
+            <hr />
+            <div className="mixPic-cont"> 
+            <img src="/mixPic.jpg" alt="mixPic Error" className="mixPic"/>
+            </div>
+        </div>
+    )
+}
---
+++ my-app/src/MainContent.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function MainContent() {
+    return(
+        <div>
+            <h1>Online Experiences</h1>
+            <p className="main-text">Different from typical tours and services, Airbnb Experiences are designed to be highly unique and interactive. So, services are generally ineligible as they don't provide expertise, insider access, and connection. Learn more about what doesn't qualify as an Airbnb Experience.</p>
+        </div>
+    )
+}
